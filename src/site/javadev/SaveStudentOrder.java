a4b4cdf2c7108db7de2ce6f23e63aa715ff38dd9
gemansipator <gemansipator@gmail.com>
2022-03-04 17:10:54 +0300

    4 end

--- src/site/javadev/SaveStudentOrder.java
+++ src/site/javadev/SaveStudentOrder.java
@@ -3,6 +3,6 @@ package site.javadev;
 public class SaveStudentOrder {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("SaveStudentOrder is running");
     }
 }
