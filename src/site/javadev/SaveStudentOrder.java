63660c39430a6260a742bee27dcc73923a0b84cc
gemansipator <gemansipator@gmail.com>
2022-03-04 17:43:12 +0300

    5 11:01

--- src/site/javadev/SaveStudentOrder.java
+++ src/site/javadev/SaveStudentOrder.java
@@ -4,12 +4,12 @@ public class SaveStudentOrder {
 
     public static void main(String[] args) {
         saveStudentOrder();
+        saveStudentOrder();
     }
 
     static void saveStudentOrder(){
         System.out.println("saveStudentOrder 1");
-        System.out.println("saveStudentOrder 2");
-        System.out.println("saveStudentOrder 3");
+
 
     }
 }
