601c7bbad56774fe9e06dc33d917b3a40d8ab21c
gemansipator <gemansipator@gmail.com>
2022-03-04 22:16:26 +0300

    6 end   Данным нужны типы.

--- src/site/javadev/SaveStudentOrder.java
+++ src/site/javadev/SaveStudentOrder.java
@@ -10,15 +10,24 @@ public class SaveStudentOrder {
         so.wFirstName = "Галина";
         so.wLastName = "Петрова";
 
+        StudentOrder so1;
+        so1 = new StudentOrder();
+        so1.hFirstName = "Алексей";
+        so1.hLastName = "Сидоров";
+        so1.wFirstName = "Галина";
+        so1.wLastName = "Сидорова";
+
         long ans = saveStudentOrder(so);
         System.out.println(ans);
+        long ans1 = saveStudentOrder(so1);
+        System.out.println(ans1);
 
     }
 
     static long saveStudentOrder(StudentOrder studentOrder){
         long ansfer = 199;
 
-        System.out.println("saveStudentOrder: " + studentOrder.hFirstName);
+        System.out.println("saveStudentOrder: " + studentOrder.hLastName);
         return ansfer;
     }
 }
